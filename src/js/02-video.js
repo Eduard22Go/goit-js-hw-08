8f1b46c2c31c1c739e596bec8c159db478a828fd
Eduard <mefoed@gmail.com>
2023-06-30 15:48:10 +0300

    Vimeo плеєр

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = document.querySelector('iframe');
+const player = new Player(vimeoPlayer);
+const LOCAL_PLAYER_KEY = 'videoplayer-current-time';
+
+let currentTime = localStorage.getItem(LOCAL_PLAYER_KEY) || 0;
+player.setCurrentTime(currentTime);
+
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem(LOCAL_PLAYER_KEY, seconds);
+    console.log(
+      'current time seconds:',
+      localStorage.getItem(LOCAL_PLAYER_KEY)
+    );
+  }, 1000)
+);
